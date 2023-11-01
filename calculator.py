e32ec41bf1f6598b37a17c8d179668d93cd39727
chris-levine <cocobean195@gmail.com>
2023-10-31 20:18:19 -0400

    uploading first two projects

---
+++ calculator.py
@@ -0,0 +1,75 @@
+# calculator project
+
+# setting inital variable to check status if calculator is still in use
+
+status = "yes"
+
+# displaying welcome message
+print("""
+---------------------------------------
+      
+    Hello, this is a calculator program!
+    Instuctions:
+        1. Enter the first number
+        2. Enter the operation (+, -, *, /)
+        3. Enter the second number
+    Enjoy this program
+
+---------------------------------------    
+""")
+
+# creating a while loop to show that we are still using the calculator 
+while status == "yes":
+
+    num1 = int(input("Enter the first number: "))
+    print(" ")
+    print("---------------------------------------")
+    print(" ")
+    operator = input("Enter the operation: ")
+    print(" ")
+    print("---------------------------------------")
+    print(" ")
+    num2 = int(input("Enter the second number: "))
+    print(" ")
+    print("---------------------------------------")
+
+    # running conditionals
+    if operator == "+":
+        print(" ")
+        print("You entered " + str(num1) + " + " + str(num2))
+        print("The answer is: " + str((num1 + num2)))
+        print(" ") 
+        status = input("Would you like to make another calculation? (yes or no) ")
+    elif operator == "-":
+        print(" ")
+        print("You entered " + str(num1) + " - " + str(num2))
+        print("The answer is: " + str((num1 - num2)))
+        print(" ") 
+        status = input("Would you like to make another calculation? (yes or no) ")
+    elif operator == "*":
+        print(" ")
+        print("You entered " + str(num1) + " * " + str(num2))
+        print("The answer is: " + str((num1 * num2)))
+        print(" ") 
+        status = input("Would you like to make another calculation? (yes or no) ")
+    elif operator == "/":
+        print(" ")
+        print("You entered " + str(num1) + " / " + str(num2))
+        print("The answer is: " + str((num1 / num2)))
+        print(" ") 
+        status = input("Would you like to make another calculation? (yes or no) ")
+    elif operator == "/" and num2 == 0:
+        print(" ")
+        print("You cannot divide by zero!")
+        print(" ")
+        status = input("Would you like to make another calculation? (yes or no) ")
+    else:
+        break
+print("---------------------------------------")
+print(" ")
+print("Thank you for using my program!")
+print(" ")
+print("---------------------------------------")
+        
+
+    
---
+++ down_payment.py
@@ -0,0 +1,62 @@
+# down payment calculator 
+
+# initial variable to track if using
+status = "yes"
+
+# displaying welcome message
+print(" ")
+print("---------------------------------------")
+print("Hello! I am here to help you with the purchase of your house.")
+print("This program will help you calculate the down payment you must make based on the price of your house!")
+print("---------------------------------------")
+
+# creating a while loop to show we are still using this calculator 
+
+while status == "yes":
+    print(" ")
+    house_price = int(input("What is the price of your house? (please enter an amount) $"))
+    print(" ")
+    down_payment = 0
+    
+    # writing condition statements 
+    if house_price >= 0 and house_price < 200000:
+        down_payment = down_payment + round(house_price * .20, 2)
+        print("A house that is $" + str(house_price) + " requires a 20% down payment!")
+        print("You will need to put $" + str(down_payment) + " down")
+        print(" ")
+        print("---------------------------------------")
+        print(" ")
+        status = input("Woud you like to enter another number? (yes or no) ")
+        print(" ")
+        print("---------------------------------------")
+    
+    elif house_price >= 200000 and house_price < 399999:
+        down_payment = down_payment + round(house_price * .15, 2)
+        print("A house that is $" + str(house_price) + " requires a 15% down payment!")
+        print("You will need to put $" + str(down_payment) + " down")
+        print(" ")
+        print("---------------------------------------")
+        print(" ")
+        status = input("Woud you like to enter another number? (yes or no) ")
+        print(" ")
+        print("---------------------------------------")
+    
+    elif house_price >= 400000: 
+        down_payment = down_payment + round(house_price * .1, 2)
+        print("A house that is $" + str(house_price) + " requires a 10% down payment!")
+        print("You will need to put $" + str(down_payment) + " down")
+        print(" ")
+        print("---------------------------------------")
+        print(" ")
+        status = input("Woud you like to enter another number? (yes or no) ")
+        print(" ")
+        print("---------------------------------------")
+
+    else:
+        print("Invalid response!")
+        print(" ")
+        status = input("Would you like to try again (yes or no) ")
+        print(" ")
+        print("---------------------------------------")
+
+print("Thank you for using this program!")
